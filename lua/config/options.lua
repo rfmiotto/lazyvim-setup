ee5573407e51e5ca612c21757dd933c9cf183f18
Renato Miotto <renato.fmiotto@gmail.com>
2025-06-15 16:50:06 -0300

    fix bad quotes display behavior

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable line wrap
+vim.opt.wrap = false
+
+-- Display lines with double quotes (LazyVim hides them, and it is annoying)
+vim.opt.conceallevel = 0
+vim.opt.concealcursor = ""
