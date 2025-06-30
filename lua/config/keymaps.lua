450d8f558b6a3086bca88cfae7968a2dea6754e7
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2025-06-30 19:32:46 -0300

    fix(keymaps): remove C-w map to avoid conflicts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,16 +12,16 @@ vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { sile
 
 vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
 
-vim.keymap.set("n", "<C-w>", function()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local listed_buffers = vim.tbl_filter(function(buf)
-    return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
-  end, vim.api.nvim_list_bufs())
-
-  if #listed_buffers > 1 then
-    vim.cmd("bprevious") -- Go to previous buffer
-    vim.cmd("bdelete " .. bufnr) -- Delete current
-  else
-    vim.cmd("q") -- Quit Neovim
-  end
-end, { desc = "Close current buffer (VSCode-like)" })
+-- vim.keymap.set("n", "<C-w>", function()
+--   local bufnr = vim.api.nvim_get_current_buf()
+--   local listed_buffers = vim.tbl_filter(function(buf)
+--     return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
+--   end, vim.api.nvim_list_bufs())
+--
+--   if #listed_buffers > 1 then
+--     vim.cmd("bprevious") -- Go to previous buffer
+--     vim.cmd("bdelete " .. bufnr) -- Delete current
+--   else
+--     vim.cmd("q") -- Quit Neovim
+--   end
+-- end, { desc = "Close current buffer (VSCode-like)" })
